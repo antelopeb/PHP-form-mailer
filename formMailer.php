6919d0ee07bf2006e0ea135f0a191718a913bf42
Greg Davis <antelope.buzkowksi@gmail.com>
2012-06-11 01:23:14 -0600

    Add a callback to the mailer to ensure success

--- formMailer.php
+++ formMailer.php
@@ -9,28 +9,28 @@ It's got a couple layers of protection after the UI and forwards to a page on su
 //custom mailer function - has some specs that need to be set by the website administrator/server administrator
 //this can be used, if there are problems with the default functionality used below
 function SendMail($ToName, $ToEmail, $FromName, $FromEmail, $Subject, $Body, $Header) {
-	$SMTP = fsockopen("smtp.sitename.com", 25);//will need to be customized
-	
-	$InputBuffer = fgets($SMTP, 1024);
-	
-	fputs($SMTP, "HELO sitename.com\n");//will need to customized
-	$InputBuffer = fgets($SMTP, 1024);
-	fputs($SMTP, "MAIL From: $FromEmail\n");
-	$InputBuffer = fgets($SMTP, 1024);
-	fputs($SMTP, "RCPT To: $ToEmail\n");
-	$InputBuffer = fgets($SMTP, 1024);
-	fputs($SMTP, "DATA\n");
-	$InputBuffer = fgets($SMTP, 1024);
-	fputs($SMTP, "$Header");
-	fputs($SMTP, "From: $FromName <$FromEmail>\n");
-	fputs($SMTP, "To: $ToName <$ToEmail>\n");
-	fputs($SMTP, "Subject: $Subject\n\n");
-	fputs($SMTP, "$Body\r\n.\r\n");
-	fputs($SMTP, "QUIT\n");
-	$InputBuffer = fgets($SMTP, 1024);
-	
-	fclose($SMTP);
-} 
+$SMTP = fsockopen("smtp.sitename.com", 25);//will need to be customized
+
+$InputBuffer = fgets($SMTP, 1024);
+
+fputs($SMTP, "HELO sitename.com\n");//will need to customized
+$InputBuffer = fgets($SMTP, 1024);
+fputs($SMTP, "MAIL From: $FromEmail\n");
+$InputBuffer = fgets($SMTP, 1024);
+fputs($SMTP, "RCPT To: $ToEmail\n");
+$InputBuffer = fgets($SMTP, 1024);
+fputs($SMTP, "DATA\n");
+$InputBuffer = fgets($SMTP, 1024);
+fputs($SMTP, "$Header");
+fputs($SMTP, "From: $FromName <$FromEmail>\n");
+fputs($SMTP, "To: $ToName <$ToEmail>\n");
+fputs($SMTP, "Subject: $Subject\n\n");
+fputs($SMTP, "$Body\r\n.\r\n");
+fputs($SMTP, "QUIT\n");
+$InputBuffer = fgets($SMTP, 1024);
+
+fclose($SMTP);
+}
 
 //this function keeps the email sanitized and prevents an injection of multiple email addresses
 function spamcheck($field)
@@ -59,28 +59,32 @@ $subject = "Your subject here";
 $noRobot = $_REQUEST['noRobot'];
 //the following headers are included in order to send a nicely formatted html message
 $headers = "From: no-reply@yourdomain.com\r\n";
-$headers.="Return-Path: no-reply@yourdomain.com\r\n"; 
+$headers.="Return-Path: no-reply@yourdomain.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 //the body of the email is declared as a long string
 $mailString = <<<EOD
-<html>
-	<body>
-		Email content goes here!
-	</body>
-</html>
+	<html>
+		<body>
+			Email content goes here!
+		</body>
+	</html>
 EOD;
 
 if (($noRobot == null) && (spamcheck($email) == true)) {
-	wp_mail($email, $subject, $mailString, $headers);
 	//echo ($mailString);//display string for proofing
-	header( 'Location: http://www.yoursite.com/success' ) ;
+	if(mail($email, $subject, $mailString, $headers)) {
+		header( 'Location: http://www.yoursite.com/success' );
+	}
+	else {
+		echo("There was an internal server error, please go back and try again.");
+	}
 }
 else {
 	//error page if something doesn't work
 	//This should never occur if the form is functioning, all errors will be prevented by the UI
-	header( 'Location: http://www.yoursite.com/failure' ) ;
+	echo("There was an internal server error, please go back and try again.");
 }
 
 ?>
